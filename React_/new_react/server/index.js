3c4f72cf2ad28ac41eef90bb1f3856852fcdd931
Himanshu8747 <himanshu8747@gmail.com>
2025-02-20 10:23:58 +0530

    Add Backend Integration

--- React_/new_react/server/index.js
+++ React_/new_react/server/index.js
@@ -1,24 +1,35 @@
-import express from 'express';
-import cors from 'cors';
-import dotenv from 'dotenv';
-import connectDB from './src/config/db.js';
-import patientRoutes from './src/routes/patientRoutes.js';
+import dotenv from "dotenv";
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import authRoutes from "./routes/authRoutes.js";
+import invoiceRoutes from "./routes/invoiceRoutes.js";
+import medicalDataRoutes from "./routes/medicalDataRoutes.js";
+import patientRoutes from "./routes/patientRoutes.js";
+import medicineRoutes from "./routes/medicineRoutes.js";
+import dashboardRoutes from "./routes/dashboardRoutes.js";
+import { fileURLToPath } from "url";
+import path from "path";
 
-dotenv.config();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+dotenv.config({ path: path.resolve(__dirname, "./.env") });
 
 const app = express();
-const PORT = process.env.PORT || 5000;
-
-// Middleware
-app.use(cors());
 app.use(express.json());
+app.use(cors());
 
-connectDB();
+// Database Connection
+mongoose.connect(process.env.MONGO_URI)
+.then(() => console.log("MongoDB connected"))
+.catch(err => console.log(err));
 
-// Routes
-app.use('/api/patients', patientRoutes);
-
-// Start the server
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+app.use("/api/auth", authRoutes);
+app.use("/api/invoices", invoiceRoutes);
+app.use("/api/medical-data", medicalDataRoutes);
+app.use("/api/patients", patientRoutes);
+app.use("/api/medicines", medicineRoutes);
+app.use("/api/dashboard", dashboardRoutes);
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
