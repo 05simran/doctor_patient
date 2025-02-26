8e21e6e96adac52263efb08e94882ea5a4e48f17
05simran <simranharry05@gmail.com>
2025-02-26 12:57:31 +0530

    file update

--- React_/new_react/new_react/server/index.js
+++ React_/new_react/new_react/server/index.js
@@ -29,12 +29,15 @@ mongoose
     .connect(process.env.MONGO_URI)
     .then(() => console.log("MongoDB connected"))
     .catch((err) => console.log(err));
-
+app.use("/", (req, res) => {
+    res.json("Hello Server");
+});
 app.use("/api/auth", authRoutes);
 app.use("/api/invoices", invoiceRoutes);
 app.use("/api/medical-data", medicalDataRoutes);
 app.use("/api/patients", patientRoutes);
 app.use("/api/medicines", medicineRoutes);
 app.use("/api/dashboard", dashboardRoutes);
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
